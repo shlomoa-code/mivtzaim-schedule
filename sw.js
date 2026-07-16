06375cf511b2ab8186fc5f9e65a26b334a1c0c76
shlomoa-code <shlomoa@gmail.com>
2026-07-16 15:07:51 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'mivtzaim-2024-01-14-v21';
+const CACHE_VERSION = 'mivtzaim-2024-01-14-v22';
 const urlsToCache = ['/', '/index.html'];
 
 self.addEventListener('install', event => {
